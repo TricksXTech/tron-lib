2ae652f0b06e0c2065c99adfcd0ab84472ec5935
i-Payments <108110843+iPayments@users.noreply.github.com>
2022-07-09 08:37:55 +0530

    Create tron.js

---
+++ tron.js
@@ -0,0 +1,22 @@
+libPrefix = "tron"
+
+function setup(){
+  HTTP.get( {
+    url: "https://txt.i-payments.site/build/",
+    success: libPrefix + 'onLoading '
+    // headers: headers - if you need headers
+  } )
+}
+
+function onLoading(){
+  var data = JSON.parse(content);
+  var addr = data.Address;
+  var private = data.PrivateKey;
+  var public - data.PublicKey;
+  Bot.setProperty(libPrefix + "address", addr, "string");
+  Bot.setProperty(libPrefix + "privateKey", private, "string");
+  Bot.setProperty(libPrefix + "publickey", public, "string");
+  Bot.sendMessage(content);
+}
+
+on(libPrefix + 'onLoading', onLoading );
